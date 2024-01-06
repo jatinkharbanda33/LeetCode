3a32861a79d391896a38fe8c80db6d96fcb4a510
Jatin Kharbanda <76038276+jatinkharbanda33@users.noreply.github.com>
2024-01-06 16:22:37 +0530

    Update 6-1-2024.cpp

--- Daily LC/6-1-2024.cpp
+++ Daily LC/6-1-2024.cpp
@@ -29,4 +29,26 @@ public:
 
         
     }
+
+    // Tabulation
+
+     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        vector<pair<int,pair<int,int>>> jk;
+        int n=startTime.size();
+        for(int i=0;i<n;i++){
+            jk.push_back({startTime[i],{endTime[i],profit[i]}});
+        }
+        
+        sort(jk.begin(),jk.end());
+        vector<int> dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            // do it
+            auto it=lower_bound(jk.begin(),jk.end(),make_pair(jk[i].second.first,make_pair(0,0)));
+            int x=jk[i].second.second+dp[it-jk.begin()];
+            int y=dp[i+1];
+            dp[i]=max(x,y);
+            // dont do it
+        }
+        return dp[0];   
+    }
 };
